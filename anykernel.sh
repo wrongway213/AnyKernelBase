d552f5c818a96a1c5d2ea1cf40426c3e06a0b91f
apophis9283 <steaven.android@gmail.com>
2018-11-13 16:33:38 -0500

    Add Yoinx to anykernel.sh. The man secretly does so much behind the lines. This is the least we (Krieg kernel) can do

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Krieg Kernel for the OnePlus 5/T by APOPHIS9283, wrongway213
+kernel.string=Krieg Kernel for the OnePlus 5/T by APOPHIS9283, wrongway213 & Yoinx
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
